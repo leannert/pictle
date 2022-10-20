37407b2f74513d7bb26454239f286aa50deb971d
markmaci <markmaci71@gmail.com>
2022-10-20 17:59:38 -0400

    listen in server.js

--- src/backend/api/server.js
+++ src/backend/api/server.js
@@ -1,9 +1,18 @@
+import express from 'express';
+
 import { getTwitchToken } from "./auth/igdb.js";
 import { getGames } from "./auth/igdb.js";
 import { getAuth } from "./auth/spotify.js";
 import { getAudioFeatures_Track } from "./auth/spotify.js";
 
-
+let server = express();
+const PORT = process.env.PORT || 8000;
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.get("/api/hello", (req, res) => {
+    res.status(200).send("Message from the server");
+  });
+  server.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
 //getTwitchToken();
 // getAuth();
@@ -12,6 +21,9 @@ getAudioFeatures_Track('1atjqOZTCdrjxjMyCPZc2g?si=oElxFD9iRASzJa8n_CkxCw&nd=1');
 
 
 // getGames();
+
+
+
 
 
 
