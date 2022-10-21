638641d86b109f2f4ab0d88cce4b955dbb024ca1
markmaci <markmaci71@gmail.com>
2022-10-21 13:27:55 -0400

    album art e2e + prettier config

--- src/backend/api/server.js
+++ src/backend/api/server.js
@@ -1,30 +1,31 @@
-import express from 'express';
+import express from 'express'
 
-import { getTwitchToken } from "./auth/igdb.js";
-import { getGames } from "./auth/igdb.js";
-import { getAuth } from "./auth/spotify.js";
-import { getAudioFeatures_Track } from "./auth/spotify.js";
+import { getTwitchToken } from './auth/igdb.js'
+import { getGames } from './auth/igdb.js'
+import { getAuth } from './auth/spotify.js'
+import { getAudioFeatures_Track } from './auth/spotify.js'
 
-let server = express();
-const PORT = process.env.PORT || 8000;
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-server.get("/api/hello", (req, res) => {
-    res.status(200).send("Message from the server");
-  });
-  server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+let server = express()
+const PORT = process.env.PORT || 8000
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+server.listen(PORT, () => console.log(`listening on port ${PORT}`))
+server.get('/api/hello', (req, res) => {
+    res.status(200).send('Message from the server')
+})
 
 //getTwitchToken();
 // getAuth();
-console.log("track data \n");
-getAudioFeatures_Track('1atjqOZTCdrjxjMyCPZc2g?si=oElxFD9iRASzJa8n_CkxCw&nd=1');
+console.log('track data \n')
+const testImage = await getAudioFeatures_Track(
+    '1atjqOZTCdrjxjMyCPZc2g?si=oElxFD9iRASzJa8n_CkxCw&nd=1'
+)
+if (testImage != null) {
+    console.log('testImage from spotify.js is ' + { testImage })
+}
 
+server.get('/dev/testimage', (req, res) => {
+    res.status(200).send({ testImage })
+})
 
 // getGames();
-
-
-
-
-
-
-
--- src/frontend/pictle/src/App.js
+++ src/frontend/pictle/src/App.js
@@ -1,17 +1,29 @@
-import React, { useEffect, useState } from "react"; 
-import "./App.css";
-import axios from "./axios";
-
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import axios from './axios'
 
 function App() {
-const [result, setResult] = useState("");
-useEffect(() => {
- axios.get("api/hello").then((response) => {
-  setResult(response.data);
- });
-}, []);
-return <div className="App">
- {result && <h1>{result}</h1>}
-</div>;
+    const [result, setResult] = useState('')
+    const [testImage, setTestImage] = useState('')
+    useEffect(() => {
+        axios.get('api/hello').then((response) => {
+            setResult(response.data)
+        })
+
+        axios.get('dev/testimage').then((response) => {
+            console.log(response.data.testImage)
+            setTestImage(response.data.testImage)
+        })
+    }, [])
+
+    return (
+        result && (
+            <>
+                <h1>{result}</h1>
+                <img src={testImage} alt="testImage" />
+            </>
+        )
+    )
 }
-export default App;
+
+export default App
