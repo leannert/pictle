65e06ac51424df8f4d40e0ae86ccf3db5d0303bf
rebecca_lawrence <rlaw01@bu.edu>
2022-11-10 18:07:09 -0500

    added basic grid

--- src/frontend/pictle/src/App.js
+++ src/frontend/pictle/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, SafeAreaView } from "react-native"
 import './App.css'
 import axios from './axios'
 
@@ -22,8 +23,47 @@ function App() {
                 <h1>{result}</h1>
                 <img src={"https:"+testImage} alt="testImage" />
             </>
+        ) && (
+            <SafeAreaView>
+                <View>
+                <GuessRow />
+                <GuessRow />
+                <GuessRow />
+                <GuessRow />
+                <GuessRow />
+                <GuessRow />
+                </View>
+            </SafeAreaView>
         )
     )
 }
+
+const GuessRow = () => (
+    <View style={styles.guessRow}>
+      <View style={styles.guessSquare}></View>
+      <View style={styles.guessSquare}></View>
+      <View style={styles.guessSquare}></View>
+      <View style={styles.guessSquare}></View>
+      <View style={styles.guessSquare}></View>
+    </View>
+  )
+
+
+const styles = StyleSheet.create({
+    guessRow: {
+      flexDirection: "row",
+      justifyContent: "center",
+    },
+    guessSquare: {
+      borderColor: "#d3d6da",
+      borderWidth: 2,
+      width: 50,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      margin: 5,
+    },
+  })
+  
 
 export default App
